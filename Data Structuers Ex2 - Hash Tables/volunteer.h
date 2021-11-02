b980dada1ec1fdfdce323bdf17d110709f62843d
Aviad <Aviad.kle@gmail.com>
2021-11-02 12:30:27 +0200

    a

--- Data Structuers Ex2 - Hash Tables/volunteer.h
+++ Data Structuers Ex2 - Hash Tables/volunteer.h
@@ -1,58 +1,68 @@
 #pragma once
 
 #include "AbstractHashTable.h"
-#include<string>
-#include<iostream>
+#include "Client.h"
+#include <string>
+#include <list>
+#include <iostream>
+
 using namespace std;
 
-class volunteer
+class Volunteer
 {
 public:
 	string name;
 	string address;
 	int phone;
-	string city;
+	//string city;
+	list <string> clients;
 
-	volunteer() {};
-	volunteer(string name_, string address_, int phone_, string city_);
-	~volunteer() {};
-	void operator=(volunteer& temp);
-	bool operator==(volunteer& temp);
+	Volunteer() {};
+	Volunteer(string name_, string address_, int phone_, string city_);
+	~Volunteer() {};
+	void operator=(Volunteer& temp);
+	bool operator==(Volunteer& temp);
 
-	friend ostream& operator<<(ostream& out, volunteer& ob); 
-	friend istream& operator>>(istream& in, volunteer& ob);	
+	friend ostream& operator<<(ostream& out, Volunteer& ob);
+	friend istream& operator>>(istream& in, Volunteer& ob);
 };
 //constructor
-volunteer::volunteer(string name_, string address_, int phone_, string city_)
+Volunteer::Volunteer(string name_, string address_, int phone_, string city_)
 {
 	name = name_;
 	address = address_;
 	phone = phone_;
-	city = city_;
+	//city = city_;
 }
 // operator =
-void volunteer::operator=(volunteer& temp)
+void Volunteer::operator=(Volunteer& temp)
 {
 	this->name = temp.name;
 	this->address = temp.address;
 	this->phone = temp.phone;
-	this->city = temp.city;
+	//this->city = temp.city;
 }
 //operator ==
-bool volunteer::operator==(volunteer& temp)
+bool Volunteer::operator==(Volunteer& temp)
 {
 	if (this->name == temp.name) return 1;
 	else return 0;
 }
 //operator cin
-istream& operator>>(istream& in, volunteer& ob)
+istream& operator>>(istream& in, Volunteer& ob)
 {
-	in >> ob.name >> ob.address >> ob.phone >> ob.city;
+	cout << "Enter volunteer name, phone, address:\n ";
+	in >> ob.name >> ob.phone >> ob.address; /*>> ob.city*/
 	return in;
 }
 //operator out
-ostream& operator<<(ostream& out, volunteer& ob)
+ostream& operator<<(ostream& out, Volunteer& ob)
 {
-	out << ob.name << ob.address << ob.phone << ob.city;
+	out << ob.name << ": volunteer name =" << ob.name << " phone =" << ob.phone << " address =" << ob.address << endl;
+	if (!(ob.clients.empty()))
+	{
+		for (list<string>::iterator iter = ob.clients.begin(); iter != ob.clients.end(); iter++)
+			cout << *iter << endl;
+	}
 	return out;
 }
